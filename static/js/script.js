348001748eaa3a94e1cf41e958c94867d856776b
marvelike <marvelike7@gmail.com>
2019-07-17 14:30:15 +0100

    speech-to-text updated

--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,24 @@
-function say(word){
-    var utter = new SpeechSynthesisUtterance(word);
-    speechSynthesis.speak(utter);
+function say(word, onEnd) {
+    const utterance = new SpeechSynthesisUtterance(word);
+    if (onEnd != null) {
+        utterance.onend = onEnd;
+    }
+    speechSynthesis.speak(utterance);
 }
 
-let webSpeech = function(words){
+function speech_recognition(onResult, onError, options) {
+    const recognition = new webkitSpeechRecognition();
+    recognition.continuous = options.continuous;
+    recognition.interimResults = options.interimResults;
 
-        let synth = window.speechSynthesis;
-        let utterance = window.SpeechSynthesisUtterance;
-        utterance.rate = 0.1;
-
-        return synth.speak(new utterance(words));
-    }
-    window.addEventListener('load',function(){
-
-     say("You can read mails, create mails, delete mails and sign out on this page.");
-    })
-
-function speech_recognition(){
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-
-    recognition.lang = 'en-Us';
+    recognition.lang = 'en-US';
     recognition.start();
 
     recognition.onresult = function (e) {
-        document.getElementById('message').value = e.results[0][0].transcript;
+        onResult(e.results[0][0].transcript);
     }
-    recognition.onerror = function(e){
+    recognition.onerror = function (e) {
         recognition.stop();
+        onError(e);
     }
-
-
 }
