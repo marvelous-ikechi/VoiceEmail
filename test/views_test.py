a5a0a6fbd4cde54277078cd16f510b94d3c64f29
marvelike <marvelike7@gmail.com>
2019-08-27 14:13:42 +0100

    version 1.0 complete

---
+++ test/views_test.py
@@ -0,0 +1,11 @@
+import unittest
+from vmail import views
+
+
+class TestIndex(unittest.TestCase):
+    def SetUp(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
